6f53128f6e1b05901b8dad13ac9e1ec3a3de894e
Sam Waller <samuellwaller@gmail.com>
2015-06-25 00:20:14 -0400

    menu and location routes up and running

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150625033340) do
+
+  create_table "locations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "menus", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
