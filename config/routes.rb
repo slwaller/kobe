dc47bd60fa3df2adc2166e086732d6ddae9902bb
Sam Waller <samuellwaller@gmail.com>
2015-06-24 23:38:31 -0400

    routes for menu and location

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,8 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+  resources :locations
+  resources :menus
   # Example resource route with options:
   #   resources :products do
   #     member do
